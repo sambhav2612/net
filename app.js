2b005c351b25b5a1550fd0efc4bc3ec31b28757f
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-09 22:03:19 +0530

    add frontend part

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
 var type = connection.type;
+var downlink = connection.downlink;
 
-console.log(' Current connection type is ' + type);
+var root = document.getElementById('root');
+var container = document.createElement('div');
+container.setAttribute('class', 'container');
+root.appendChild(container);
+
+var para = document.createElement('p');
+para.setAttribute('id', 'main-text');
+container.appendChild(para);
+
+console.log('Current connection type is ' + type + ' with bandwidth ' + connection.bandwidth + ' and downlink ' + downlink + 'Mb/s');
+para.innerHTML = 'Current connection type is ' + type + ' with bandwidth ' + connection.bandwidth + ' and downlink ' + downlink + 'Mb/s';
 
 function updateConnection() {
-    console.log('Connection switched from ' + type + ' to' + connection.type);
+    console.log('Connection switched from ' + type + ' to ' + connection.type);
+    para.innerHTML = 'Connection switched from ' + type + ' to ' + connection.type;
 }
 
+connection.onchange = updateConnection;
 connection.addEventListener('typechange', updateConnection);
